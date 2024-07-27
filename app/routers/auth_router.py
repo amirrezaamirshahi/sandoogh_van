035a46e562f640cef9157f0ae71f33a80346985c
Amirreza Amirshahi <amirreza2002amirshahi@gmail.com>
2024-07-27 14:43:16 +0430

    test

--- app/routers/auth_router.py
+++ app/routers/auth_router.py
@@ -4,10 +4,12 @@ from fastapi.security import OAuth2PasswordRequestForm
 from pymongo.errors import DuplicateKeyError
 from app.models.models import UserCreate, User
 from app.database.database import get_user_collection
-from app.auth.auth import get_password_hash, verify_password, create_access_token, ACCESS_TOKEN_EXPIRE_MINUTES  # اضافه کردن ایمپورت ثابت
+# اضافه کردن ایمپورت ثابت
+from app.auth.auth import get_password_hash, verify_password, create_access_token, ACCESS_TOKEN_EXPIRE_MINUTES
 import uuid  # برای تولید کد عضویت یونیک
 
 router = APIRouter(prefix="/auth", tags=["auth"])
+
 
 @router.post("/register", response_model=User)
 def register(user: UserCreate):
@@ -18,34 +20,40 @@ def register(user: UserCreate):
     user_data.pop("password")
     user_data.pop("confirm_password")
     user_data["hashed_password"] = hashed_password
-    user_data["created_at"] = datetime.utcnow()  # اضافه کردن زمان فعلی به عنوان تاریخ ایجاد
+    # اضافه کردن زمان فعلی به عنوان تاریخ ایجاد
+    user_data["created_at"] = datetime.utcnow()
     user_data["membership_code"] = str(uuid.uuid4())  # تولید کد عضویت یونیک
 
     # Check for unique username
     if get_user_collection().find_one({"username": user.username}):
-        raise HTTPException(status_code=400, detail="Username already registered")
-    
+        raise HTTPException(
+            status_code=400, detail="Username already registered")
+
     try:
         get_user_collection().insert_one(user_data)
     except DuplicateKeyError:
         raise HTTPException(status_code=400, detail="Email already registered")
     return User(**user_data)
 
+
 @router.post("/token")
 def login(form_data: OAuth2PasswordRequestForm = Depends(), remember_me: bool = Form(False)):
     user = get_user_collection().find_one({"username": form_data.username})
     if not user or not verify_password(form_data.password, user["hashed_password"]):
-        raise HTTPException(status_code=401, detail="Incorrect username or password")
+        raise HTTPException(
+            status_code=401, detail="Incorrect username or password")
 
     token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     if remember_me:
-        token_expires = timedelta(days=30)
+        token_expires = timedelta(days=1)
 
-    access_token = create_access_token(data={"sub": user["username"]}, expires_delta=token_expires)
+    access_token = create_access_token(
+        data={"sub": user["username"]}, expires_delta=token_expires)
     user_data = {
         "username": user["username"],
         "email": user["email"],
-        "created_at": user["created_at"],  # اضافه کردن تاریخ ایجاد به داده‌های کاربر
+        # اضافه کردن تاریخ ایجاد به داده‌های کاربر
+        "created_at": user["created_at"],
         "status": user["status"],  # اضافه کردن وضعیت کاربر
         "membership_code": user["membership_code"]  # اضافه کردن کد عضویت کاربر
     }
