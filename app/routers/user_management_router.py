593eb34adc017f4fbb1bb8d7b7234f0c8dcc5870
Amirreza Amirshahi <amirreza2002amirshahi@gmail.com>
2024-07-30 10:36:25 +0430

    user-serach

--- app/routers/user_management_router.py
+++ app/routers/user_management_router.py
@@ -1,8 +1,9 @@
 # routers/user_management_router.py
-from fastapi import APIRouter, HTTPException, Depends
+from fastapi import APIRouter, HTTPException, Depends, Query
 from app.models.models import UserResponse
 from app.database.database import get_user_collection
-from app.routers.user_router import get_current_user  # برای دسترسی به کاربر فعلی
+from app.routers.user_router import get_current_user
+from typing import Optional
 
 router = APIRouter(prefix="/user-management", tags=["user-management"])
 
@@ -23,7 +24,45 @@ def get_all_users():
         "status": 1
     }))
 
-    # تبدیل ObjectId به رشته
+    for user in users:
+        user['_id'] = str(user['_id'])
+
+    return users
+
+@router.get("/search", response_model=list[UserResponse], dependencies=[Depends(admin_only)])
+def search_users(
+    first_name: Optional[str] = Query(None, description="نام"),
+    last_name: Optional[str] = Query(None, description="نام خانوادگی"),
+    username: Optional[str] = Query(None, description="نام کاربری"),
+    status: Optional[str] = Query(None, description="وضعیت"),
+    user_type: Optional[str] = Query(None, description="نوع کاربر"),
+    category: Optional[str] = Query(None, description="دسته")
+):
+    query = {}
+
+    if first_name:
+        query["first_name"] = {"$regex": first_name, "$options": "i"}
+    if last_name:
+        query["last_name"] = {"$regex": last_name, "$options": "i"}
+    if username:
+        query["username"] = {"$regex": username, "$options": "i"}
+    if status:
+        query["status"] = status
+    if user_type:
+        query["user_type"] = user_type
+    if category:
+        query["category"] = category
+
+    users = list(get_user_collection().find(query, {
+        "first_name": 1,
+        "last_name": 1,
+        "membership_code": 1,
+        "username": 1,
+        "user_type": 1,
+        "created_at": 1,
+        "status": 1
+    }))
+
     for user in users:
         user['_id'] = str(user['_id'])
 
